dcebe9a4c4d76834771703cabcfe152d9eb4bac2
Atanas Arshinkov <aarshinkov9705@gmail.com>
2023-09-11 16:00:09 +0300

    Initial commit

---
+++ src/main/java/com/aarshinkov/template/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.aarshinkov.template.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Atanas Yordanov Arshinkov
+ * @since 1.0.0
+ */
+@Controller
+public class HomeController {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @GetMapping(value = "/")
+  public String home() {
+    return "home";
+  }
+}
---
+++ src/main/java/com/aarshinkov/template/utils/AppConstants.java
@@ -0,0 +1,14 @@
+package com.aarshinkov.template.utils;
+
+/**
+ *
+ * @author Atanas Yordanov Arshinkov
+ * @since 1.0.0
+ */
+public class AppConstants {
+
+  public static final String LANG_DEFAULT = "bg";
+  public static final String LANG_EN = "en";
+  public static final String LANG_BG = "bg";
+  public static final String LANG_COOKIE_NAME = "lang";
+}
